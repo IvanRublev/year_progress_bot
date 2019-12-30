ce8fd6a5fb7e9bd9e0830748b92b086eab412f95
Ivan Rublev <ivan@ivanrublev.me>
2019-12-30 09:31:21 +0100

    Add empty project

---
+++ README.md
@@ -0,0 +1,9 @@
+year_progress_bot
+=====
+
+A Telegram bot that shows a progress bar for the current year.
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ apps/year_progress_bot/src/year_progress_bot_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc year_progress_bot public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(year_progress_bot_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    year_progress_bot_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
