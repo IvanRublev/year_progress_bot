7ce95287bc427c811759a1f1407aaf245b67d68d
Ivan Rublev <ivan@ivanrublev.me>
2020-01-05 16:38:23 +0100

    Add setup to tests

--- apps/year_progress_bot/test/notifyer_sup_tests.erl
+++ apps/year_progress_bot/test/notifyer_sup_tests.erl
@@ -2,11 +2,15 @@
 -include_lib("eunit/include/eunit.hrl").
 
 should_evaluate_send_of_progress_periodically_test() ->
-    meck:new(notifyer, [{stub_all, ok}]),
+    {setup,
+    fun() -> ok end,
+    fun(_) -> meck:unload(notifyer) end,
+    fun(_) ->
+        meck:new(notifyer, [{stub_all, ok}]),
 
-    Pid = spawn(notifyer_sup, loop, [1]),
-    receive after 10 -> ok end,
-    exit(Pid, exit),
+        Pid = spawn(notifyer_sup, loop, [1]),
+        receive after 10 -> ok end,
+        exit(Pid, exit),
 
-    ?assert(meck:num_calls(notifyer, evaluate_send_progress, '_') >= 3),
-    meck:unload(notifyer).
+        ?_assert(meck:num_calls(notifyer, evaluate_send_progress, '_') >= 3)
+    end}.
--- apps/year_progress_bot/test/notifyer_tests.erl
+++ apps/year_progress_bot/test/notifyer_tests.erl
@@ -1,22 +1,35 @@
 -module(notifyer_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-should_send_progress_to_subscribed_chats_after_11_30_CET_test() ->
-    meck:new(telegram, [{stub_all, ok}]),
-    meck:new(local_time, [{stub_all, {11, 30}}]),
+should_send_progress_to_subscribed_chats_after_11_30_CET_test_() ->
+    {setup,
+     fun() -> ok end,
+     fun(_) -> 
+        meck:unload(local_time),
+        meck:unload(telegram)
+     end,
+     fun(_) -> 
+        meck:new(telegram, [{stub_all, ok}]),
+        meck:new(local_time, [{stub_all, {11, 30}}]),
 
-    notifyer:evaluate_send_progress(),
+        notifyer:evaluate_send_progress(),
 
-    ?assert(meck:called(telegram, send_message, '_')),
-    meck:unload(local_time),
-    meck:unload(telegram).
+        ?_assert(meck:called(telegram, send_message, '_'))
+     end}.
 
-should_Not_send_progress_to_subscribed_chats_before_11_30_CET_test() ->
-    meck:new(telegram, [{stub_all, ok}]),
-    meck:new(local_time, [{stub_all, {11, 29}}]),
+should_Not_send_progress_to_subscribed_chats_before_11_30_CET_test_() ->
+    {setup,
+    fun() -> ok end,
+    fun(_) ->
+        meck:unload(local_time),
+        meck:unload(telegram)        
+    end,
+    fun(_) ->
+        meck:new(telegram, [{stub_all, ok}]),
+        meck:new(local_time, [{stub_all, {11, 29}}]),
 
-    notifyer:evaluate_send_progress(),
+        notifyer:evaluate_send_progress(),
 
-    ?assertMatch(false, meck:called(telegram, send_message, '_')),
-    meck:unload(local_time),
-    meck:unload(telegram).
+        ?_assertMatch(false, meck:called(telegram, send_message, '_'))
+    end}.
+
