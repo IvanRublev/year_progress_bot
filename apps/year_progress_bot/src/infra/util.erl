4bed5b04d729c67aa5fc64f2b6ef807223829ef9
Ivan Rublev <ivan@ivanrublev.me>
2020-01-06 18:33:50 +0100

    Test pause to work correctly

--- apps/year_progress_bot/src/infra/util.erl
+++ apps/year_progress_bot/src/infra/util.erl
@@ -1,5 +1,17 @@
 -module(util).
 -export([pause/1]).
 
-pause(_) ->
-    ok.
+pause(T) ->
+    timer:sleep(round(T)).
+
+%---------------
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+should_stop_process_for_given_milliseconds_test() ->
+    {MicroSec, _} = timer:tc(util, pause, [100.0]),
+    Err = 10000,
+    ?assert(MicroSec >= (100000-Err)),
+    ?assert(MicroSec =< (100000+Err)).
+
+-endif.
