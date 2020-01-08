75e5346b26136823b78e5ecd487bc64ceeb988b7
Ivan Rublev <ivan@ivanrublev.me>
2020-01-08 08:55:46 +0100

    Encode messgae payload

--- apps/year_progress_bot/src/telegram.erl
+++ apps/year_progress_bot/src/telegram.erl
@@ -1,6 +1,7 @@
 -module(telegram).
 -export([send_message/2]).
 
-send_message(_ChatId, ProgressDate) ->
+send_message(ChatId, ProgressDate) ->
     Msg = formatter:year_progress_bar(ProgressDate),
+    Payload = jiffy:encode({[{chat_id, ChatId}, {text, Msg}]}),
     ok.
--- apps/year_progress_bot/test/telegram_tests.erl
+++ apps/year_progress_bot/test/telegram_tests.erl
@@ -5,14 +5,23 @@ send_test_() ->
     {foreach,
      fun() ->
          meck:new(formatter),
-         meck:expect(formatter, year_progress_bar, fun(_) -> "%" end)
+         meck:expect(formatter, year_progress_bar, fun(_) -> "%" end),
+         meck:new(jiffy),
+         meck:expect(jiffy, encode, fun(_) -> "{json}" end)
      end,
      fun(_) ->
+         meck:unload(jiffy),
          meck:unload(formatter)
      end,
-     [fun should_get_progress_bar_from_date/1]}.
+     [fun should_get_progress_bar_from_date/1,
+     fun should_make_json_payload_for_progress_bar_message/1]}.
 
 should_get_progress_bar_from_date(_) ->
     telegram:send_message(0, {{2020,10,11}, {11,50}}),
     
     ?_assert(meck:called(formatter, year_progress_bar, [{{2020,10,11}, {11,50}}])).
+
+should_make_json_payload_for_progress_bar_message(_) ->
+    telegram:send_message(15, {{2020,10,11}, {11,50}}),
+
+    ?_assert(meck:called(jiffy, encode, [{[{chat_id, 15}, {text, "%"}]}])).
